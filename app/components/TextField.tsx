a9fbe94957c9d78de2ac16cb26deb5821047c5c3
Jeongbin1 <petitjb05@gamil.com>
2025-01-20 18:39:56 +0900

    💄 design: 공통 TextField 반응형 breakpoint  수정

--- app/components/TextField.tsx
+++ app/components/TextField.tsx
@@ -25,7 +25,7 @@ export default function TextField({
 
   const typeStyle =
     type === 'box'
-      ? `border border-[#F8FAFC1A] px-[16px] md:px-[24px] py-[8px] md:py-[16px] text-md md:text-lg leading-26 focus:border-bd-primary border-1px rounded-[12px] bg-b-secondary placeholder-[#9CA3AF] overflow-auto ${styles.customScrollbar}`
+      ? `border border-[#F8FAFC1A] px-[16px]  sm:px-[24px] py-[8px] sm:py-[16px] text-md sm:text-lg leading-26 focus:border-bd-primary border-1px rounded-[12px] bg-b-secondary placeholder-[#9CA3AF] overflow-auto ${styles.customScrollbar}`
       : 'h-full p-0 text-sm border-none bg-transparent placeholder-t-default';
 
   // reply 타입 - 내용에 맞게 높이 설정
