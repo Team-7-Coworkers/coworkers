90e8e04526cfebabecf19cf1dd6a7480293527ce
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-01-20 15:49:14 +0900

    ✨ feat: storybook main.js 설정 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,29 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import path from 'path';
 
-const config: StorybookConfig = {
+module.exports = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ["..\\public"],
+  staticDirs: ['../public'], // 경로 수정: '\\' 대신 '/' 사용
+
+  // Webpack 설정 추가
+  webpackFinal: async (config) => {
+    // next/image를 모킹 파일로 대체
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'next/image': path.resolve(__dirname, '../__mock__/next/image'),
+    };
+    return config;
+  },
 };
-export default config;
