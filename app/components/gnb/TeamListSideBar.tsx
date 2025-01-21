12b031f0ce0e9aaa99aa4d688aa1d17567fd7f5c
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-01-21 10:57:43 +0900

    ✨ feat: gnb SideBar 구현

---
+++ app/components/gnb/TeamListSideBar.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { mockTeamList } from './TeamListDropDown';
+
+export default function TeamListSideBar({
+  onClose,
+  isOpen
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  return (
+    <nav
+      className={`fixed left-0 top-0 h-full bg-gray-800 text-white shadow-lg transition-transform duration-300 ${
+        isOpen ? 'translate-x-0' : '-translate-x-full'
+      } z-50 w-52 space-y-10 p-4 sm:hidden`}
+    >
+      <div
+        className="w-full cursor-pointer text-end"
+        onClick={onClose}
+      >
+        X {/*임시*/}
+      </div>
+      <div className="space-y-4 text-md">
+        {mockTeamList.map((team) => (
+          <div key={team.id}>{team.name}</div>
+        ))}
+      </div>
+    </nav>
+  );
+}
