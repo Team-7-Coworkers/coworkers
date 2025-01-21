3846cf9ee59dd833c4e3af2eb084fe37dc9f84a9
Jeongbin1 <petitjb05@gamil.com>
2025-01-21 10:31:19 +0900

    🚑️ fix: 공통 TextField 스크롤바 밀림, 창 크기 변경 시 오류 수정

--- app/components/TextField.tsx
+++ app/components/TextField.tsx
@@ -21,18 +21,31 @@ export default function TextField({
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const baseStyle =
-    'w-full text-primary resize-none font-normal text-t-primary focus:outline-none';
+    'w-full text-primary resize-none break-all font-normal text-t-primary focus:outline-none';
 
   const typeStyle =
     type === 'box'
-      ? `border border-[#F8FAFC1A] px-[16px]  sm:px-[24px] py-[8px] sm:py-[16px] text-md sm:text-lg leading-26 focus:border-bd-primary border-1px rounded-[12px] bg-b-secondary placeholder-[#9CA3AF] overflow-auto ${styles.customScrollbar}`
-      : 'h-full p-0 text-sm border-none bg-transparent placeholder-t-default';
+      ? `border border-[#F8FAFC1A] pl-[16px] sm:pl-[24px] py-[8px] sm:py-[16px] text-md sm:text-lg leading-26 focus:border-bd-primary border-1px rounded-[12px] bg-b-secondary placeholder-[#9CA3AF] ${styles.customScrollbar}`
+      : 'h-full p-0 text-sm border-none bg-transparent placeholder-t-default overflow-hidden';
 
   // reply 타입 - 내용에 맞게 높이 설정
+  const adjustHeight = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto'; // 먼저 높이를 자동으로 재설정
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // 실제 내용에 맞는 높이로 설정
+    }
+  };
+
   useEffect(() => {
-    if (textareaRef.current && type === 'reply') {
-      textareaRef.current.style.height = '0px';
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    if (type === 'reply') {
+      adjustHeight();
+
+      // 창 크기 변경 시에도 높이 조절
+      window.addEventListener('resize', adjustHeight);
+
+      return () => {
+        window.removeEventListener('resize', adjustHeight); // 리소스 정리
+      };
     }
   }, [value, type]);
 
