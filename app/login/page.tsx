3390a174ff7f1bca3fa68cd1db4f523ee7c09a9e
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-01-23 01:44:48 +0900

    ✨ feat: 로그인 성공시 accessToken 저장 로직 연결
    - userStore 내 변수명 변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,8 +4,31 @@ import React from 'react';
 import LoginForm from '@/app/login/LoginForm';
 import { postAuthSignIn } from '../api/auth.api';
 import axios from 'axios';
+import useUserStore from '../stores/userStore';
+
+interface LoginSuccessPayload {
+  accessToken: string;
+  user: {
+    id: number;
+    email: string;
+    nickname: string;
+    updatedAt: string;
+    createdAt: string;
+    image: string | null;
+    teamId: string;
+  };
+}
 
 export default function LoginPage() {
+  const { setToken, setUser } = useUserStore();
+
+  const handleLoginSuccess = ({ accessToken, user }: LoginSuccessPayload) => {
+    setToken(accessToken);
+    setUser(user);
+
+    console.log('로그인 성공:', user);
+  };
+
   const handleLoginSubmit = async (formData: {
     email: string;
     password: string;
@@ -14,9 +37,11 @@ export default function LoginPage() {
 
     try {
       const response = await postAuthSignIn({ email, password });
+      const { accessToken, user } = response;
 
       console.log(response);
       alert('로그인에 성공했습니다!');
+      handleLoginSuccess({ accessToken, user });
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
         const errorMessage = error.response.data.message;
