4bc21d5930e421a96aad37b7548d337f2147a5a0
석지우 <jiwoo9665@gmail.com>
2025-01-22 20:18:36 +0900

    💄 design: 호버 애니메이션 추가

--- app/boards/Card.tsx
+++ app/boards/Card.tsx
@@ -13,7 +13,7 @@ export default function Card() {
   useEffect(() => {
     const loadCard = async () => {
       try {
-        const articles = await getArticles({}); // 파라미터 생략
+        const articles = await getArticles({});
         setCard(articles);
       } catch (error) {
         console.error('카드 로드 실패:', error);
@@ -28,18 +28,29 @@ export default function Card() {
         card.list.map((article) => (
           <div
             key={article.id}
-            className="flex h-[176px] w-[589px] cursor-pointer flex-col rounded-[12px] border border-gray-700 bg-b-secondary px-[32px] py-[24px] transition-transform duration-200 hover:translate-y-[-7px]"
+            className="flex h-[176px] w-[589px] cursor-pointer flex-col rounded-[12px] border border-gray-700 bg-b-secondary px-[32px] py-[24px] transition-transform duration-300 hover:scale-105"
           >
             {/* Title and Image */}
             <div className="flex justify-between">
-              <p className="text-[18px] text-t-secondary">{article.title}</p>
-              <Image
-                src={article.image}
-                alt={article.title}
-                width={72}
-                height={72}
-                className="h-[72px] w-[72px] rounded-[8px] object-cover"
-              />
+              <p
+                className="overflow-hidden text-[18px] text-t-secondary"
+                style={{
+                  display: '-webkit-box',
+                  WebkitBoxOrient: 'vertical',
+                  WebkitLineClamp: 3,
+                }}
+              >
+                {article.title}
+              </p>
+              {article.image !== 'https://no-image/no-image.png' && (
+                <Image
+                  src={article.image}
+                  alt={article.title}
+                  width={72}
+                  height={72}
+                  className="h-[72px] w-[72px] rounded-[8px] object-cover pl-[10px]"
+                />
+              )}
             </div>
 
             {/* Additional Information */}
