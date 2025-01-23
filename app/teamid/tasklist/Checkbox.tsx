9184d2cdbe8baa8af8a7285872d79338f5195cda
1022gusl <1022gusl@gmail.com>
2025-01-23 16:21:48 +0900

    💄 design: 리스트페이지 체크박스 크기 변경

--- app/teamid/tasklist/Checkbox.tsx
+++ app/teamid/tasklist/Checkbox.tsx
@@ -8,18 +8,18 @@ export default function Checkbox({
   onChange: (id: number) => void;
 }) {
   return (
-    <div className="relative h-5 w-5">
+    <div className="relative flex h-4 w-4 items-center justify-center">
       <input
         type="checkbox"
         checked={checked}
         onChange={() => onChange(id)}
-        className="h-5 w-5 cursor-pointer appearance-none rounded-md border border-ic-inverse checked:border-transparent checked:bg-tertiary focus:outline-none"
+        className="h-full w-full cursor-pointer appearance-none rounded-md border border-ic-inverse checked:border-transparent checked:bg-tertiary focus:outline-none"
       />
       {checked && (
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
           <svg
-            width="16"
-            height="16"
+            width="12"
+            height="12"
             viewBox="0 0 16 16"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
