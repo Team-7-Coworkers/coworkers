e3a4632ddc7db643382da32a268ec62b6fc11e28
Jeongbin1 <petitjb05@gamil.com>
2025-01-23 13:47:00 +0900

    💄 design: 드롭다운 버튼 classname 속성 추가

--- app/components/Dropdown/DropdownButton.tsx
+++ app/components/Dropdown/DropdownButton.tsx
@@ -3,13 +3,19 @@ import React, { ReactNode } from 'react';
 interface DropdownButtonProps {
   children: ReactNode;
   onClick: () => void;
+  className?: string;
 }
 
-const DropdownButton = ({ children, onClick }: DropdownButtonProps) => {
+const DropdownButton = ({
+  children,
+  onClick,
+  className,
+}: DropdownButtonProps) => {
   return (
     <button
       type="button"
       onClick={onClick}
+      className={className}
     >
       {children}
     </button>
