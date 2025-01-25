d5280703a5f9925a647dce537a832df5b097d584
석지우 <jiwoo9665@gmail.com>
2025-01-26 00:19:38 +0900

    ✨ feat: 사용자 여부에 따라 드롭다운 안 보이게 설정

--- app/boards/CardList.tsx
+++ app/boards/CardList.tsx
@@ -9,7 +9,7 @@ import { useState } from 'react';
 import Pagination from './Pagination';
 import Dropdown from '@/app/components/Dropdown';
 import PostActionDropdown from './PostActionDropdown';
-//import useUserStore from '@/app/stores/userStore';
+import useUserStore from '@/app/stores/userStore';
 import Link from 'next/link';
 
 export default function CardList({
@@ -21,7 +21,7 @@ export default function CardList({
   orderBy?: 'recent' | 'like';
   hideItem?: boolean;
 }) {
-  //const { user } = useUserStore();
+  const { user } = useUserStore();
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [orderByDropdown, setOrderByDropdown] = useState<'recent' | 'like'>(
@@ -102,74 +102,77 @@ export default function CardList({
       {/* 카드 리스트 */}
       <div className="grid gap-[24px] sm:grid-cols-1 lg:grid-cols-2">
         {articles && articles.list.length > 0 ? (
-          articles.list.map((article) => (
-            <Link
-              key={article.id}
-              href={`/boards/${article.id}`}
-              className="block"
-              onClick={(e) => {
-                // Prevent default navigation when dropdown is clicked
-                if (
-                  e.target instanceof HTMLElement &&
-                  e.target.closest('.dropdown')
-                ) {
-                  e.preventDefault();
-                }
-              }}
-            >
-              <div
+          articles.list.map((article) => {
+            const isOwner = user?.nickname === article.writer.nickname;
+
+            return (
+              <Link
                 key={article.id}
-                className="flex h-[175px] cursor-pointer flex-col rounded-[12px] border border-gray-700 bg-b-secondary px-[32px] py-[24px] transition-transform duration-300 hover:scale-105 hover:bg-b-tertiary"
+                href={`/boards/${article.id}`}
+                className="pointer-events-auto block"
               >
-                <div className="flex justify-between">
-                  {/* Title */}
-                  <p className="mr-[8px] line-clamp-2 max-h-[48px] flex-grow overflow-hidden text-[18px] leading-[1.5] text-t-secondary">
-                    {article.title}
-                  </p>
+                <div
+                  className="flex h-[175px] cursor-pointer flex-col rounded-[12px] border border-gray-700 bg-b-secondary px-[32px] py-[24px] transition-transform duration-300 hover:scale-105 hover:bg-b-tertiary"
+                  onClick={(e) => {
+                    const target = e.target as HTMLElement;
+                    if (target.closest('.dropdown')) {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }
+                  }}
+                >
+                  <div className="flex justify-between">
+                    <p className="mr-[8px] line-clamp-2 max-h-[48px] flex-grow overflow-hidden text-[18px] leading-[1.5] text-t-secondary">
+                      {article.title}
+                    </p>
 
-                  {article.image !== 'https://no-image/no-image.png' && (
-                    <div className="relative ml-[16px] h-[72px] w-[72px] flex-shrink-0 overflow-hidden rounded-[8px]">
-                      <Image
-                        src={article.image}
-                        alt={article.title}
-                        fill
-                        className="object-cover"
-                      />
+                    {article.image !== 'https://no-image/no-image.png' && (
+                      <div className="relative ml-[16px] h-[72px] w-[72px] flex-shrink-0 overflow-hidden rounded-[8px]">
+                        <Image
+                          src={article.image}
+                          alt={article.title}
+                          fill
+                          className="object-cover"
+                        />
+                      </div>
+                    )}
+
+                    {/* Dropdown */}
+                    {isOwner && (
+                      <div className="dropdown pointer-events-auto ml-[16px] flex-shrink-0">
+                        <PostActionDropdown
+                          onEdit={() => console.log(`수정: ${article.id}`)}
+                          onDeleteSuccess={() =>
+                            console.log('삭제 완료 후 상태 업데이트')
+                          }
+                          articleId={article.id}
+                        />
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="mt-auto flex items-center justify-between">
+                    <div className="flex">
+                      <p className="mr-[32px] text-[14px] text-t-primary">
+                        {article.writer.nickname}
+                      </p>
+                      <p className="text-[14px] text-t-disabled">
+                        {dayjs(article.createdAt).format('YYYY.MM.DD')}
+                      </p>
                     </div>
-                  )}
-
-                  <div className="dropdown ml-[16px] flex-shrink-0">
-                    <PostActionDropdown
-                      onEdit={() => console.log(`수정: ${article.id}`)}
-                      onDeleteSuccess={() =>
-                        console.log('삭제 완료 후 상태 업데이트')
-                      }
-                      articleId={article.id}
-                    />
+                    <div className="flex items-center text-t-disabled">
+                      <Image
+                        src={HeartIcon}
+                        alt="heartIcon"
+                        className="mr-1 h-4 w-4"
+                      />
+                      <p className="text-[14px]">{article.likeCount}</p>
+                    </div>
                   </div>
                 </div>
-
-                <div className="mt-auto flex items-center justify-between">
-                  <div className="flex">
-                    <p className="mr-[32px] text-[14px] text-t-primary">
-                      {article.writer.nickname}
-                    </p>
-                    <p className="text-[14px] text-t-disabled">
-                      {dayjs(article.createdAt).format('YYYY.MM.DD')}
-                    </p>
-                  </div>
-                  <div className="flex items-center text-t-disabled">
-                    <Image
-                      src={HeartIcon}
-                      alt="heartIcon"
-                      className="mr-1 h-4 w-4"
-                    />
-                    <p className="text-[14px]">{article.likeCount}</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))
+              </Link>
+            );
+          })
         ) : (
           <p>로딩중...</p>
         )}
