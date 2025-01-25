e41892ffccb8cf2898f05158228f9237066b0066
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-01-26 05:25:16 +0900

    ✨ feat: frequencyType 케이스 추가

--- app/types/shared/task.d.ts
+++ app/types/shared/task.d.ts
@@ -2,6 +2,23 @@ import { UserProfileType } from './user';
 
 export type FrequencyType = 'ONCE' | 'MONTHLY' | 'WEEKLY' | 'DAILY';
 
+export type FrequencySpecificParams =
+  | {
+      frequencyType: 'MONTHLY'; // MONTHLY일 때 monthDay 필수
+      monthDay: number;
+      weekDays?: never; // weekDays는 허용하지 않음
+    }
+  | {
+      frequencyType: 'WEEKLY'; // WEEKLY일 때 weekDays 필수
+      weekDays: number[]; // 0~6의 배열
+      monthDay?: never; // monthDay는 허용하지 않음
+    }
+  | {
+      frequencyType: 'DAILY' | 'ONCE'; // DAILY와 ONCE는 둘 다 추가 필드 없음
+      monthDay?: never;
+      weekDays?: never;
+    };
+
 export interface TaskType {
   doneBy: {
     user: UserProfileType;
