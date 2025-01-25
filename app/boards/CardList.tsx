bb60a59c95c8f6c37a124127a78e69083f0bf01d
석지우 <jiwoo9665@gmail.com>
2025-01-25 22:36:39 +0900

    ✨ feat: 드롭다운 눌렀을 때 페이지 이동 안 되게 수정

--- app/boards/CardList.tsx
+++ app/boards/CardList.tsx
@@ -102,77 +102,74 @@ export default function CardList({
       {/* 카드 리스트 */}
       <div className="grid gap-[24px] sm:grid-cols-1 lg:grid-cols-2">
         {articles && articles.list.length > 0 ? (
-          articles.list.map((article) => {
-            //const isOwner = user?.nickname === article.writer.nickname;
-
-            return (
-              <Link
+          articles.list.map((article) => (
+            <Link
+              key={article.id}
+              href={`/boards/${article.id}`}
+              className="block"
+              onClick={(e) => {
+                // Prevent default navigation when dropdown is clicked
+                if (
+                  e.target instanceof HTMLElement &&
+                  e.target.closest('.dropdown')
+                ) {
+                  e.preventDefault();
+                }
+              }}
+            >
+              <div
                 key={article.id}
-                href={`/boards/${article.id}`}
-                className="block"
+                className="flex h-[175px] cursor-pointer flex-col rounded-[12px] border border-gray-700 bg-b-secondary px-[32px] py-[24px] transition-transform duration-300 hover:scale-105 hover:bg-b-tertiary"
               >
-                <div
-                  key={article.id}
-                  className="flex h-[175px] cursor-pointer flex-col rounded-[12px] border border-gray-700 bg-b-secondary px-[32px] py-[24px] transition-transform duration-300 hover:scale-105 hover:bg-b-tertiary"
-                >
-                  <div className="flex justify-between">
-                    {/* Title */}
-                    <p className="mr-[8px] line-clamp-2 max-h-[48px] flex-grow overflow-hidden text-[18px] leading-[1.5] text-t-secondary">
-                      {article.title}
-                    </p>
+                <div className="flex justify-between">
+                  {/* Title */}
+                  <p className="mr-[8px] line-clamp-2 max-h-[48px] flex-grow overflow-hidden text-[18px] leading-[1.5] text-t-secondary">
+                    {article.title}
+                  </p>
 
-                    {/* Image */}
-                    {article.image !== 'https://no-image/no-image.png' && (
-                      <div className="relative ml-[16px] h-[72px] w-[72px] flex-shrink-0 overflow-hidden rounded-[8px]">
-                        <Image
-                          src={article.image}
-                          alt={article.title}
-                          fill
-                          className="object-cover"
-                        />
-                      </div>
-                    )}
-
-                    {/* Dropdown */}
-                    <div className="ml-[16px] flex-shrink-0">
-                      {/*
-                  {isOwner && (
-                  */}
-                      <PostActionDropdown
-                        onEdit={() => console.log(`수정: ${article.id}`)}
-                        onDeleteSuccess={() =>
-                          console.log('삭제 완료 후 상태 업데이트')
-                        }
-                        articleId={article.id}
-                      />
-                      {/*
-                  )}
-                  */}
-                    </div>
-                  </div>
-
-                  <div className="mt-auto flex items-center justify-between">
-                    <div className="flex">
-                      <p className="mr-[32px] text-[14px] text-t-primary">
-                        {article.writer.nickname}
-                      </p>
-                      <p className="text-[14px] text-t-disabled">
-                        {dayjs(article.createdAt).format('YYYY.MM.DD')}
-                      </p>
-                    </div>
-                    <div className="flex items-center text-t-disabled">
+                  {article.image !== 'https://no-image/no-image.png' && (
+                    <div className="relative ml-[16px] h-[72px] w-[72px] flex-shrink-0 overflow-hidden rounded-[8px]">
                       <Image
-                        src={HeartIcon}
-                        alt="heartIcon"
-                        className="mr-1 h-4 w-4"
+                        src={article.image}
+                        alt={article.title}
+                        fill
+                        className="object-cover"
                       />
-                      <p className="text-[14px]">{article.likeCount}</p>
                     </div>
+                  )}
+
+                  <div className="dropdown ml-[16px] flex-shrink-0">
+                    <PostActionDropdown
+                      onEdit={() => console.log(`수정: ${article.id}`)}
+                      onDeleteSuccess={() =>
+                        console.log('삭제 완료 후 상태 업데이트')
+                      }
+                      articleId={article.id}
+                    />
                   </div>
                 </div>
-              </Link>
-            );
-          })
+
+                <div className="mt-auto flex items-center justify-between">
+                  <div className="flex">
+                    <p className="mr-[32px] text-[14px] text-t-primary">
+                      {article.writer.nickname}
+                    </p>
+                    <p className="text-[14px] text-t-disabled">
+                      {dayjs(article.createdAt).format('YYYY.MM.DD')}
+                    </p>
+                  </div>
+                  <div className="flex items-center text-t-disabled">
+                    <Image
+                      src={HeartIcon}
+                      alt="heartIcon"
+                      className="mr-1 h-4 w-4"
+                    />
+                    <p className="text-[14px]">{article.likeCount}</p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))
         ) : (
           <p>로딩중...</p>
         )}
