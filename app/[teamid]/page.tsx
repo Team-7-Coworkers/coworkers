c75c408bb907515ca00c93f7f7db25ca7466a71d
haksoo <haksoo0918@gmail.com>
2025-01-29 16:53:23 +0900

    🔨 refactor: reactQuery 적용, 드롭다운 적용

--- app/[teamid]/page.tsx
+++ app/[teamid]/page.tsx
@@ -1,18 +1,22 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { notFound } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
 
-import { getGroups } from '../api/group.api';
-import type { groupResponseType } from '../types/group';
+import { getGroups, deleteGroups } from '../api/group.api';
+import useUserStore from '../stores/userStore';
+import { GroupResponseType } from '../types/group';
 
 import TaskLists from './TaskLists';
 import MemberList from './MemberList';
 import Report from './Report';
+import Dropdown from '../components/Dropdown';
+import Modal, { ModalFooter } from '../components/Modal';
+import Button from '../components/Button';
 
 import GearIcon from '../components/icons/GearIcon';
 import styles from './teampage.module.css';
-// import useUserStore from '../stores/userStore';
 
 interface Props {
   params: {
@@ -21,79 +25,109 @@ interface Props {
 }
 
 export default function TeamPage({ params }: Props) {
-  const [data, setData] = useState<groupResponseType | null>(null);
+  const [teamid, setTeamid] = useState('');
   const [role, setRole] = useState('');
   const [totalTasks, setTotalTasks] = useState(0);
   const [completedTasks, setCompletedTasks] = useState(0);
-  // const { user } = useUserStore(); // 아직 못가져옴
-
-  // TODO: user store 에서 가져와야 함
-  const D_USER_ID = 1375;
+  const [deleteModal, setDeleteModal] = useState(false);
+  const { user } = useUserStore();
+  const router = useRouter();
 
   useEffect(() => {
-    // 팀 아이디 값으로 그룹정보 받는 함수
-    async function fetchGroups() {
+    // teamid 설정
+    async function fetchParams() {
       const { teamid } = await params;
-      try {
-        const result: groupResponseType = await getGroups({
-          id: Number(teamid),
-        });
-        // console.log('result:', result);
-        if (result !== null) {
-          setData(result);
-          // user id로 role 역할 받기
-          const member = result.members.find(
-            (member) => member.userId === D_USER_ID
-          );
-          // console.log('member:', member);
-          setRole(member.role);
-
-          let total = 0;
-          let complete = 0;
-          // 할 일 전체와 완료된 할 일 갯수 받기
-          result.taskLists.forEach((taskList) => {
-            total += taskList.tasks.length;
-            complete += taskList.tasks.filter(
-              (task) => task.doneAt !== null
-            ).length;
-          });
-          setTotalTasks(total);
-          setCompletedTasks(complete);
-        } else {
-          // TODO: 방향성 협의 필요
-          // 404로 다이렉트
-          notFound();
-        }
-      } catch (err) {
-        console.log('--- 그룹 정보 패치: err:', err);
-        // 404로 다이렉트
-        notFound();
-      }
+      setTeamid(teamid);
     }
 
-    fetchGroups();
+    fetchParams();
   }, [params]);
 
-  if (data === null) {
-    return <div>Loading...</div>;
+  const {
+    data: group,
+    isPending,
+    isError,
+  } = useQuery<GroupResponseType['getGroups']>({
+    queryKey: ['groupId', teamid],
+    queryFn: async () => await getGroups({ groupId: Number(teamid) }),
+    enabled: !!teamid,
+  });
+
+  useEffect(() => {
+    if (group) {
+      // 사용자 검증 및 역할 받아 설정
+      const member = group.members.find((member) => member.userId === user?.id);
+      // console.log('--- member:', member);
+      if (member) setRole(member.role);
+
+      // 할 일 전체와 완료된 할 일 갯수 받기
+      let total = 0;
+      let complete = 0;
+      group.taskLists.forEach((taskList) => {
+        total += taskList.tasks.length;
+        complete += taskList.tasks.filter(
+          (task) => task.doneAt !== null
+        ).length;
+      });
+      setTotalTasks(total);
+      setCompletedTasks(complete);
+    }
+  }, [group, user]);
+
+  // 팀 수정하기 버튼 클릭 함수
+  const handleModifyClick = () => {
+    router.push(`/${group?.id}/modify`);
+  };
+
+  // 팀 삭제하기 버튼 클릭
+  const handleDeleteClick = () => {
+    setDeleteModal(true);
+  };
+
+  // 팀 진짜로 삭제하기 버튼 클릭
+  const handleRealDeleteClick = async () => {
+    if (group) {
+      const result = await deleteGroups({ groupId: group?.id });
+      console.log('--- deleteGroups:result:', result);
+      router.replace('/');
+    }
+  };
+
+  if (isPending) {
+    // TODO: Loading 머지되면 적용
+    return <div className="my-4 flex justify-center p-8">Loading...</div>;
+  }
+
+  if (isError || !group) {
+    router.replace('/');
+    return null;
   }
 
   return (
     <div className="container flex flex-col py-6">
       <header className={styles.header}>
         <h1 className="text-xl font-bold text-t-inverse">
-          {data.name}({data.id})
+          {group.name}({group.id})
         </h1>
 
-        {/* TODO: 드롭다운 적용 */}
-        <button>
-          <GearIcon />
-        </button>
+        <Dropdown className="h-6">
+          <Dropdown.Button>
+            <GearIcon />
+          </Dropdown.Button>
+          <Dropdown.Menu className="right-0">
+            <Dropdown.MenuItem onClick={handleModifyClick}>
+              수정하기
+            </Dropdown.MenuItem>
+            <Dropdown.MenuItem onClick={handleDeleteClick}>
+              삭제하기
+            </Dropdown.MenuItem>
+          </Dropdown.Menu>
+        </Dropdown>
       </header>
 
       <TaskLists
-        groupId={data.id}
-        taskLists={data.taskLists}
+        groupId={group.id}
+        taskLists={group.taskLists}
       />
 
       {role === 'ADMIN' && (
@@ -104,10 +138,37 @@ export default function TeamPage({ params }: Props) {
       )}
 
       <MemberList
-        groupId={data.id}
-        members={data.members}
+        groupId={group.id}
+        members={group.members}
         role={role}
       />
+
+      <Modal
+        isOpen={deleteModal}
+        title="팀을 정말로 삭제하실 건가요?"
+        onClose={() => setDeleteModal(false)}
+        icon="danger"
+      >
+        <p className="text-pretty break-keep text-center">
+          모든 할 일 목록 및 할 일은 삭제처리 됩니다.
+        </p>
+
+        <ModalFooter>
+          <Button
+            styleType="outlined-secondary"
+            onClick={() => setDeleteModal(false)}
+          >
+            닫기
+          </Button>
+
+          <Button
+            state="danger"
+            onClick={handleRealDeleteClick}
+          >
+            팀 삭제
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
