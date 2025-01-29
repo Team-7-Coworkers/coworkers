44c76020ce5ceec549b55db940c474cd57e9cfb1
Jeongbin1 <petitjb05@gamil.com>
2025-01-29 22:41:09 +0900

    💄 design: 랜딩페이지 애니메이션 추가

--- app/components/Landing/ServiceIntro.tsx
+++ app/components/Landing/ServiceIntro.tsx
@@ -1,43 +1,21 @@
 'use client';
+
 import Image from 'next/image';
-import landingMainSmall from '@/public/images/landing/landing-main-small.png';
-import landingMainMedium from '@/public/images/landing/landing-main-medium.png';
-import landingMainLarge from '@/public/images/landing/landing-main-large.png';
+import landingMain from '@/public/images/landing/landing-main.png';
 import RepairIcon from '@/public/images/icons/ic_repair.svg';
-import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 export default function ServiceIntro() {
-  const [imageSrc, setImageSrc] = useState(landingMainSmall);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 640) {
-        setImageSrc(landingMainSmall);
-      } else if (window.innerWidth <= 1024) {
-        setImageSrc(landingMainMedium);
-      } else {
-        setImageSrc(landingMainLarge);
-      }
-    };
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <div className="relative w-full text-center">
       <Image
-        src={imageSrc}
+        src={landingMain}
         alt=""
         layout="responsive"
         priority
       />
-      <div className="absolute top-[55px] flex w-full flex-col items-center gap-1 sm:top-[100px] sm:gap-2 lg:top-[84px] lg:gap-5">
+      <div className="absolute top-[55px] flex w-full flex-col items-center gap-1 sm:top-[30px] sm:gap-2 lg:top-[44px] lg:gap-5">
         <div className="flex items-center gap-1 sm:gap-4 lg:gap-6">
           <h1 className="text-2xl font-semibold text-t-primary sm:text-4xl lg:text-[48px] lg:leading-[57.28px]">
             함께 만들어가는 투두 리스트
@@ -48,14 +26,25 @@ export default function ServiceIntro() {
             className="h-7 w-7 sm:h-12 sm:w-12 lg:h-16 lg:w-16"
           />
         </div>
-        <h1 className="bg-gradient-to-r from-primary to-tertiary bg-clip-text text-3xl font-semibold text-transparent sm:text-[48px] sm:leading-[57.28px] lg:text-[64px] lg:leading-[76.38px]">
-          Cowerkers
-        </h1>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h1 className="bg-gradient-to-r from-primary to-tertiary bg-clip-text text-3xl font-semibold text-transparent sm:text-[48px] sm:leading-[57.28px] lg:text-[64px] lg:leading-[76.38px]">
+            Cowerkers
+          </h1>
+        </motion.div>
       </div>
       <Link href="/login">
-        <button className="relative bottom-12 w-[343px] rounded-[32px] bg-gradient-to-r from-primary to-tertiary py-[13px] text-lg font-semibold sm:bottom-[120px] sm:w-[373px] sm:py-[14.5px]">
+        <motion.button
+          className="relative bottom-12 w-[343px] rounded-[32px] bg-gradient-to-r from-primary to-tertiary py-[13px] text-lg font-semibold sm:bottom-[70px] sm:w-[373px] sm:py-[14.5px]"
+          whileHover={{ scale: 1.15 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+        >
           지금 시작하기
-        </button>
+        </motion.button>
       </Link>
     </div>
   );
--- app/components/Landing/ServiceOutro.tsx
+++ app/components/Landing/ServiceOutro.tsx
@@ -1,35 +1,11 @@
 'use client';
 import Image from 'next/image';
-import landingBottomSmall from '@/public/images/landing/landing-bottom-small.png';
-import landingBottomMedium from '@/public/images/landing/landing-bottom-medium.png';
-import landingBottomLarge from '@/public/images/landing/landing-bottom-large.png';
-import { useEffect, useState } from 'react';
+import landingBottom from '@/public/images/landing/landing-bottom.png';
 
 export default function ServiceOut() {
-  const [imageSrc, setImageSrc] = useState(landingBottomSmall);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 640) {
-        setImageSrc(landingBottomSmall);
-      } else if (window.innerWidth <= 1024) {
-        setImageSrc(landingBottomMedium);
-      } else {
-        setImageSrc(landingBottomLarge);
-      }
-    };
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <div className="relative w-full text-center">
-      <div className="absolute top-[123px] flex w-full flex-col items-center gap-4 text-t-primary sm:top-[176px] sm:gap-2 lg:top-[230px] lg:gap-6">
+      <div className="absolute top-[50px] flex w-full flex-col items-center gap-4 text-t-primary sm:top-[76px] sm:gap-2 lg:top-[130px] lg:gap-6">
         <p className="text-2xl font-semibold sm:text-4xl">
           지금 바로 시작해보세요
         </p>
@@ -42,7 +18,7 @@ export default function ServiceOut() {
         </p>
       </div>
       <Image
-        src={imageSrc}
+        src={landingBottom}
         alt=""
         layout="responsive"
       />
