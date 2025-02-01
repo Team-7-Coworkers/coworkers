2671764c065c58fe7353164d2c43ea9423873b7c
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-02-01 15:47:59 +0900

    ✨ feat: kakao sdk 설정 스크립트 추가

---
+++ app/types/global.d.ts
@@ -0,0 +1,36 @@
+export {};
+
+declare global {
+  interface Window {
+    Kakao: KakaoSDK;
+  }
+}
+
+// 카카오 SDK 인터페이스 정의
+interface KakaoSDK {
+  init: (key: string) => void;
+  isInitialized: () => boolean;
+  Auth: {
+    login: (options: {
+      success: (authObj: KakaoAuthResponse) => void;
+      fail: (error: KakaoAuthError) => void;
+    }) => void;
+    logout: (callback?: () => void) => void;
+  };
+}
+
+// 로그인 성공 응답 타입
+interface KakaoAuthResponse {
+  access_token: string;
+  refresh_token?: string;
+  expires_in?: number;
+  refresh_token_expires_in?: number;
+  scope?: string;
+  token_type?: string;
+}
+
+// 카카오 로그인 실패(에러) 응답 타입
+interface KakaoAuthError {
+  error: string;
+  error_description: string;
+}
