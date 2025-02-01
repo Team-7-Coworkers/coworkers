70d784ff7243bac925b0b001990d42453d326ec0
Jeongbin1 <petitjb05@gamil.com>
2025-02-01 09:51:57 +0900

    ✨ feat: 팀 소속 없을 때 페이지 구현

---
+++ app/noteam/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Image from 'next/image';
+import Button from '../components/Button';
+import bgNoTeam from '@/public/images/bg-no-team.png';
+import Link from 'next/link';
+
+export default function NoTeamPage() {
+  return (
+    <div className="mt-[190px] px-8 sm:px-[112px] lg:mx-auto">
+      <div className="flex flex-col items-center gap-8 sm:gap-12">
+        <Image
+          src={bgNoTeam}
+          alt=""
+          width={810}
+          height={255}
+          sizes="(max-width: 810px) 100vw, 810px"
+        />
+        <p className="text-center text-md text-t-default lg:text-lg">
+          아직 소속된 팀이 없습니다.
+          <br />
+          팀을 생성하거나 팀에 참여해 보세요
+        </p>
+      </div>
+      <div className="mt-[48px] flex flex-col items-center gap-2 sm:mt-[80px] lg:gap-4">
+        <Link href="/addteam">
+          <Button size="w-[186px] h-[48px] rounded-xl">팀 생성하기</Button>
+        </Link>
+        <Link href="/invitation">
+          <Button size="w-[186px] h-[48px] rounded-xl bg-transparent border border-primary text-[#10B981] hover:border-i-pressed hover:bg-i-inactive hover:bg-opacity-15 active:border-i-pressed active:bg-[#94A3B8] active:bg-opacity-5">
+            팀 참여하기
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
