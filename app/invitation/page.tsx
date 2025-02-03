1b5c89379f97ff276da432bf42f859e0fc395c82
haksoo <haksoo0918@gmail.com>
2025-02-03 22:09:37 +0900

    🚑️ fix: build 오류로 suspense 로 useSearchParams 컴포넌트 감쌈

--- app/invitation/page.tsx
+++ app/invitation/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 
@@ -9,14 +9,19 @@ import useUserStore from '../stores/userStore';
 
 import InputField from '../components/InputField';
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 import styles from '../styles/team.module.css';
 
-export default function InvitationPage() {
-  const [link, setLink] = useState('');
+const InvitationContent = () => {
   const searchParams = useSearchParams();
+  const token = searchParams.get('t');
+
+  const [link, setLink] = useState(token || '');
   const router = useRouter();
   const { user } = useUserStore();
+
+  const validLink = link.length > 0;
 
   const { mutate } = useMutation({
     mutationFn: (email: string) =>
@@ -30,13 +35,10 @@ export default function InvitationPage() {
     },
   });
 
-  const token = searchParams.get('t');
-  const validLink = link.length > 0;
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const GET_TOKEN_IDX = '?t=';
+    const GET_TOKEN_IDX = 't=';
     const index = link.indexOf(GET_TOKEN_IDX);
 
     // input에 링크정보 들어왔을 경우 토큰정보로 변경
@@ -53,11 +55,6 @@ export default function InvitationPage() {
     }
     mutate(user.email);
   };
-
-  useEffect(() => {
-    // input 에 토큰 정보 입력
-    if (token) setLink(token);
-  }, [token]);
 
   return (
     <div className={styles.area}>
@@ -90,5 +87,13 @@ export default function InvitationPage() {
         </div>
       </form>
     </div>
+  );
+};
+
+export default function InvitationPage() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <InvitationContent />
+    </Suspense>
   );
 }
