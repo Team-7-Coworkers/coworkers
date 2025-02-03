ea024d0158881d055ee421b277e6d7fd8cbc8c72
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-02-03 19:52:03 +0900

    ✨ feat: 비밀전호 재설정 이메일 전송 로직 추가

--- app/login/ResetPasswordEmailModal.tsx
+++ app/login/ResetPasswordEmailModal.tsx
@@ -3,6 +3,10 @@ import InputField from '../components/InputField';
 import Modal, { ModalFooter } from '../components/Modal';
 import { validateEmail } from '../utils/formValidators';
 import Button from '../components/Button';
+import { useMutation } from '@tanstack/react-query';
+import { postUserSendRestPasswordEmail } from '../api/user.api';
+import { UserResponseType } from '../types/user';
+import axios from 'axios';
 
 interface ResetPasswordEmailModalProps {
   isOpen: boolean;
@@ -15,8 +19,33 @@ export default function ResetPasswordEmailModal({
 }: ResetPasswordEmailModalProps) {
   const [email, setEmail] = useState('');
 
+  const sendResetPasswordLinkMutation = useMutation({
+    mutationFn: postUserSendRestPasswordEmail,
+    onSuccess: (data: UserResponseType['postUserSendRestPasswordEmail']) => {
+      const { message } = data;
+      alert(message);
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        const errorMessage =
+          error.response?.data?.message ||
+          '오류가 발생했습니다. 다시 시도해주세요.';
+        alert(`로그인 실패: ${errorMessage}`);
+      } else {
+        alert('예기치 못한 오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
+      }
+    },
+  });
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
+  };
+
+  const handleSendEmail = () => {
+    sendResetPasswordLinkMutation.mutate({
+      email,
+      redirectUrl: 'http://localhost:3000',
+    });
   };
 
   return (
@@ -38,7 +67,7 @@ export default function ResetPasswordEmailModal({
       </div>
       <ModalFooter>
         <Button styleType="outlined">취소</Button>
-        <Button>링크 보내기</Button>
+        <Button onClick={handleSendEmail}>링크 보내기</Button>
       </ModalFooter>
     </Modal>
   );
