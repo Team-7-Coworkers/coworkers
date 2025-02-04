b66b2fbcccf6d63b85de5d3919213bfb6c6cd115
석지우 <jiwoo9665@gmail.com>
2025-02-04 17:57:07 +0900

    ✨ feat: api 추가하여 기능 추가

--- app/boards/write/page.tsx
+++ app/boards/write/page.tsx
@@ -1,29 +1,59 @@
 'use client';
 
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Button from '@/app/components/Button';
-
 import ImageUpload from '@/app/components/ImageUpload';
 import InputField from '@/app/components/InputField';
 import TextField from '@/app/components/TextField';
+import { postArticles } from '@/app/api/article.api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-export default function write() {
-  const handleUploadSuccess = () => {
-    console.log('이미지 업로드 성공');
+export default function Write() {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState('');
+
+  const handleUploadSuccess = (uploadedImageUrl: string) => {
+    setImage(uploadedImageUrl);
   };
 
-  const handleUploadError = () => {
-    console.log('이미지 업로드 실패');
+  const handleUploadError = () => {};
+
+  const postArticleMutation = useMutation({
+    mutationFn: postArticles,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['articles'] });
+      router.push('/boards');
+    },
+  });
+
+  const handleSubmit = () => {
+    if (!title.trim() || !content.trim()) {
+      alert('제목과 내용을 입력해주세요.');
+      return;
+    }
+
+    postArticleMutation.mutate({
+      title,
+      content,
+      //이미지 없이 등록할 경우 임시 주소 전송
+      image: image || 'https://no-image/no-image.png',
+    });
   };
 
   return (
     <div className="relative mx-auto flex w-[90%] flex-col items-start pb-12 sm:w-[90%] lg:w-[65%]">
-      {/* 제목 & 버튼 (PC에서만 보임) */}
       <div className="flex w-full items-center justify-between border-b border-gray-700 pb-8 pt-12">
         <p className="pb-6 text-[20px] font-bold">게시글 쓰기</p>
         <Button
           styleType="solid"
           size="w-[184px] h-[42px]"
-          classname="hidden sm:inline-block" // ✅ PC에서만 보이게 설정
+          classname="hidden sm:inline-block"
+          onClick={handleSubmit}
         >
           등록
         </Button>
@@ -33,9 +63,11 @@ export default function write() {
         <span className="text-tertiary">*</span>제목
       </p>
       <InputField
-        id={''}
-        type={'text'}
-        placeholder={'제목을 입력해주세요.'}
+        id="title"
+        type="text"
+        placeholder="제목을 입력해주세요."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
 
       <p className="pb-4 pt-8">
@@ -45,7 +77,8 @@ export default function write() {
         type="box"
         placeholder="내용을 입력해주세요."
         height={240}
-        value={''}
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
       />
 
       <p className="pb-4 pt-8">
@@ -57,10 +90,12 @@ export default function write() {
         variant="square"
       />
 
+      {/*모바일버전버튼 */}
       <Button
         styleType="solid"
         size="w-full h-[42px]"
         classname="sm:hidden mt-10"
+        onClick={handleSubmit}
       >
         등록
       </Button>
