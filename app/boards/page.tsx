ad3000054cea5336349b77e2d69593b5c2a4ae84
석지우 <jiwoo9665@gmail.com>
2025-02-04 16:26:22 +0900

    ✨ feat:  자유 게시판에 글 쓰기 버튼 추가

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -5,6 +5,7 @@ import CardList from './CardList';
 import SearchBar from './SearchBar';
 import { useState } from 'react';
 import Link from 'next/link';
+import Button from '@/app/components/Button';
 
 export default function Boards() {
   const [searchKeyword, setSearchKeyword] = useState('');
@@ -14,7 +15,7 @@ export default function Boards() {
   };
 
   return (
-    <div className="mx-auto flex w-[90%] flex-col items-center sm:w-[90%] lg:w-[65%]">
+    <div className="relative mx-auto flex w-[90%] flex-col items-center sm:w-[90%] lg:w-[65%]">
       <div className="w-full">
         <div className="flex items-center justify-between pb-8 pt-10">
           <p className="text-[20px] font-bold">베스트 게시글</p>
@@ -29,6 +30,18 @@ export default function Boards() {
         <SearchBar onSearch={handleSearch} />
         <CardList keyword={searchKeyword} />
       </div>
+
+      <Link href="/boards/write">
+        <div className="right fixed bottom-20 right-[calc(50%-45%)] z-50 sm:right-[calc(50%-45%)] lg:right-[calc(50%-32.5%)]">
+          <Button
+            styleType="solid"
+            size="w-[100px] h-[45px]"
+            classname="transition-all duration-300 ease-in-out hover:translate-y-[-10px] rounded-3xl"
+          >
+            + 글쓰기
+          </Button>
+        </div>
+      </Link>
     </div>
   );
 }
---
+++ app/boards/write/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function write() {
+  return (
+    <div className="mx-auto flex w-[90%] flex-col items-center sm:w-[90%] lg:w-[65%]">
+      <p className="pb-6 text-[20px] font-bold">댓글달기</p>
+    </div>
+  );
+}
