5fb5d590043d9a8df171d411cbd8d0691b6ed1d4
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-02-05 12:24:47 +0900

    ✨ feat: objectFit -> ovject-cover 클래스로 변경

--- app/components/Gnb/TeamListDropDownItem.tsx
+++ app/components/Gnb/TeamListDropDownItem.tsx
@@ -10,12 +10,12 @@ export default function TeamListDropDownItem({
 }: TeamListDropDownItemProps) {
   return (
     <div className="flex items-center gap-3">
-      <div className="relative h-8 w-8 overflow-hidden rounded-md">
+      <div className="relative size-8 overflow-hidden rounded-md">
         <Image
           src={team.image || '/images/icons/ic_team.svg'}
-          alt=""
+          alt="팀 이미지"
           fill
-          objectFit="cover"
+          className="object-cover"
         />
       </div>
       <p className="truncate text-lg">{team.name}</p>
