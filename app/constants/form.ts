cfc8c6ad484d1c0c46b7c1154657c4f2fca8cdd1
1022gusl <1022gusl@gmail.com>
2025-02-07 17:58:10 +0900

    ✨ feat: 인풋 글자제한

--- app/constants/form.ts
+++ app/constants/form.ts
@@ -1,8 +1,8 @@
 export const MAX_LENGTH = {
   teamName: 30,
   nickName: 50,
-  taskName: 100,
-  taskListName: 50,
-  taskMemo: 255,
-  taskComment: 255,
+  taskName: 30, // 할 일 제목 30자 최대
+  taskListName: 30, // 할 일 목록 제목 30자 최대
+  taskMemo: 255, // 할 일 메모 255자 최대
+  taskComment: 255, // 할 일 댓글 255자 최대
 };
