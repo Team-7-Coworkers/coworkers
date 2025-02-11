96de90d8b1372bf1cffb1e9ceb4b5e07eda831ea
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-02-12 00:40:00 +0900

    ✨ feat: 간편 로그인 로딩 페이지 구현

---
+++ app/oauth/EasyLoginLoadingPage.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Loading from '../components/Loading';
+
+interface EasyLoginLoadingPageProps {
+  type: 'kakao' | 'google';
+}
+
+const loginType = {
+  kakao: { name: '카카오톡', src: '/images/icons/ic_kakaotalk.svg' },
+  google: { name: '구글', src: '/images/icons/ic_google.svg' },
+};
+
+export default function EasyLoginLoadingPage({
+  type,
+}: EasyLoginLoadingPageProps) {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-4 py-40 sm:gap-6">
+      <div className="space-y-4 text-center sm:space-y-6">
+        <div className="flex items-center gap-2">
+          <div className="relative h-8 w-8 sm:h-10 sm:w-10">
+            <Image
+              src={loginType[type].src}
+              alt=""
+              fill
+            />
+          </div>
+          <p className="text-2xl font-semibold sm:text-3xl">
+            {`${loginType[type].name} 간편 로그인`}
+          </p>
+        </div>
+        <div className="space-y-1 text-xs sm:text-md">
+          <p>처리중입니다. 잠시만 기다려주세요...</p>
+        </div>
+      </div>
+      <Loading />
+    </div>
+  );
+}
