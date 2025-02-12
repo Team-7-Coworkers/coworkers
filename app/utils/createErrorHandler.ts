c1af9b4ab63b989fbac5f4510fa195171d821ccb
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-02-12 20:22:10 +0900

    ✨ feat: createErrorHandler 기본 에러 메시지 옵션 추가

--- app/utils/createErrorHandler.ts
+++ app/utils/createErrorHandler.ts
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 
 interface ErrorHandlerOptions {
   prefixMessage?: string;
+  defaultErrorMessage?: string;
   onDisplay?: (message: string) => void;
 }
 
@@ -10,10 +11,11 @@ export function createErrorHandler(options?: ErrorHandlerOptions) {
   const {
     prefixMessage = '에러',
     onDisplay = (message: string) => toast.error(message),
+    defaultErrorMessage = '오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
   } = options || {};
 
   return (error: unknown) => {
-    let message: string = '오류가 발생했습니다. 잠시 후 다시 시도해주세요.';
+    let message: string = defaultErrorMessage;
 
     if (axios.isAxiosError(error) && error.response?.data?.message) {
       message = error.response?.data?.message;
