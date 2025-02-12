2489a63bfee52138abcf8fdcdb72dd4a2be88786
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-02-12 12:22:30 +0900

    🔨 refactor: 반응형 이미지 적용 방법 변경

--- app/oauth/EasyLoginLoadingPage.tsx
+++ app/oauth/EasyLoginLoadingPage.tsx
@@ -17,11 +17,13 @@ export default function EasyLoginLoadingPage({
     <div className="flex w-full flex-col items-center justify-center gap-4 py-40 sm:gap-6">
       <div className="space-y-4 text-center sm:space-y-6">
         <div className="flex items-center gap-2">
-          <div className="relative h-8 w-8 sm:h-10 sm:w-10">
+          <div className="relative h-10 w-10">
             <Image
               src={loginType[type].src}
               alt=""
-              fill
+              width={32}
+              height={32}
+              className="sm:h-10 sm:w-10"
             />
           </div>
           <p className="text-2xl font-semibold sm:text-3xl">
