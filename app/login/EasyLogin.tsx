9ceb18d0627902a8dc05c2c77bd47276ee4f0e3a
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-02-12 17:25:37 +0900

    🔨 refactor: alert -> toast 변경

--- app/login/EasyLogin.tsx
+++ app/login/EasyLogin.tsx
@@ -2,31 +2,31 @@
 
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
+import { toast } from 'react-toastify';
 
 export default function EasyLogin({ page }: { page: 'login' | 'signup' }) {
   const handleKakaoLogin = () => {
     if (!window.Kakao) {
-      alert('카카오 SDK가 로드되지 않았습니다.');
+      toast.error('카카오 SDK가 로드되지 않았습니다.');
+
       return;
     }
     if (!window.Kakao.Auth) {
-      alert('카카오 Auth 모듈이 로드되지 않았습니다.');
+      toast.error('카카오 Auth 모듈이 로드되지 않았습니다.');
+
       return;
     }
 
-    // CSRF 방지를 위한 랜덤 값
-    // 인가 코드와 함께 돌려받았을 때 일치 여부를 검증
     const state = Math.random().toString(36).substring(2, 10);
 
     window.Kakao.Auth.authorize({
-      redirectUri: process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI!, // 인가 코드 받을 페이지
+      redirectUri: process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI!,
       state: state,
     });
 
-    localStorage.setItem('kakao_state', state); // 이후 검증을 위해 저장
+    localStorage.setItem('kakao_state', state);
   };
 
-  // 구글 로그인 창으로 이동
   const handleGoogleLogin = () => {
     signIn('google', {
       callbackUrl: '/oauth/google',
