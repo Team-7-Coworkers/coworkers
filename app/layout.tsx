9a523034f947d7f161dd3a6a9935aa46bc2d2b03
haksoo <haksoo0918@gmail.com>
2025-02-13 20:22:29 +0900

    🚑️ fix: 웹폰트를 css에서 html head로 옮김

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css"
+        />
+      </head>
       <body className="scroll-smooth pt-[var(--header-height)]">
         <Providers>
           {children}
