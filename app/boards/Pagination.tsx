56bbece9b313e67fc3573a116e572ff0ad3cca33
석지우 <jiwoo9665@gmail.com>
2025-02-13 16:52:30 +0900

    💄 design: 페이지네이션 첫페이지, 마지막 페이지일 때 화살표 버튼 디자인 추가

--- app/boards/Pagination.tsx
+++ app/boards/Pagination.tsx
@@ -25,25 +25,37 @@ export default function Pagination({
   return (
     <div className="mt-6 flex justify-center space-x-2 py-8">
       <button
-        className="rounded-md border border-gray-700 bg-b-primary px-3 py-2"
+        className={`rounded-md border px-3 py-2 ${
+          currentPage === 1
+            ? 'cursor-not-allowed border-gray-600 bg-gray-700'
+            : 'border-gray-700 bg-b-primary'
+        }`}
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
       >
         ◁
       </button>
+
       {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
         <button
           key={page}
-          className={`rounded-md border border-gray-700 px-4 py-2 ${
-            page === currentPage ? 'bg-primary' : 'bg-b-primary'
+          className={`rounded-md border px-4 py-2 ${
+            page === currentPage
+              ? 'border-gray-700 bg-primary text-white'
+              : 'border-gray-700 bg-b-primary text-gray-300'
           }`}
           onClick={() => onPageChange(page)}
         >
           {page}
         </button>
       ))}
+
       <button
-        className="rounded-md border border-gray-700 bg-b-primary px-3 py-2"
+        className={`rounded-md border px-3 py-2 ${
+          currentPage === totalPages
+            ? 'cursor-not-allowed border-gray-600 bg-gray-700'
+            : 'border-gray-700 bg-b-primary'
+        }`}
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
       >
