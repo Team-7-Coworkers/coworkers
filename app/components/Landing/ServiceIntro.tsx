00da7bbf236e8085069863926423a8125d3fbb98
Jeongbin1 <petitjb05@gamil.com>
2025-02-17 17:39:33 +0900

    🚑️ fix: 이미지 로딩 후 애니메이션 실행하도록 변경

--- app/components/Landing/ServiceIntro.tsx
+++ app/components/Landing/ServiceIntro.tsx
@@ -8,11 +8,12 @@ import { motion } from 'framer-motion';
 import useUserStore from '@/app/stores/userStore';
 import useTeamStore from '@/app/stores/teamStore';
 import { IMAGE_SIZES } from '@/app/constants/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function ServiceIntro() {
   const { user } = useUserStore();
   const { teamList, currentTeam } = useTeamStore();
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const getRedirectPath = () => {
     if (!user) return '/login';
@@ -31,7 +32,7 @@ export default function ServiceIntro() {
       <motion.div
         className="relative h-[640px] overflow-hidden sm:h-[940px] lg:h-[1080px]"
         initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
+        animate={isImageLoaded ? { opacity: 1, y: 0 } : {}}
         transition={{ duration: 0.8 }}
         viewport={{ once: true }}
       >
@@ -42,6 +43,7 @@ export default function ServiceIntro() {
           alt=""
           priority
           sizes={IMAGE_SIZES}
+          onLoadingComplete={() => setIsImageLoaded(true)}
         />
       </motion.div>
       <div className="absolute top-[55px] flex w-full flex-col items-center gap-1 sm:top-[100px] sm:gap-2 lg:top-[84px] lg:gap-5">
