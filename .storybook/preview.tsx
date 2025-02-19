c22f6c264e50d9bad461746ad99d79487e9dee75
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-02-19 18:03:36 +0900

    ✨ feat: 스토리북 tsconfig path 반영 플러그인 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { Preview } from '@storybook/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import '@styles/globals.css';
+import '../app/styles/globals.css';
 
 const queryClient = new QueryClient();
 
